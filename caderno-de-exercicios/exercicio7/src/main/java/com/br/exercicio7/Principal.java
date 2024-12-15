aeb57f8d088bc51d07ab12d4956e22b66a94a041
Tetzdesen <Tetzdesen@gmail.com>
2024-12-15 18:12:43 -0300

    Refatoração principal

--- caderno-de-exercicios/exercicio7/src/main/java/com/br/exercicio7/Principal.java
+++ caderno-de-exercicios/exercicio7/src/main/java/com/br/exercicio7/Principal.java
@@ -8,7 +8,7 @@ public class Principal {
 
     public static void main(String[] args) {
         try {
-            Cliente cliente = new Cliente("Kurt Cobain", 25, 10, 1, 2, 2);
+            Cliente cliente = new Cliente("Kurt Cobain", 36, 11, 0, 2, 2);
 
             AvaliadoraSeguroService avaliadoraSeguro = new AvaliadoraSeguroService();
             Seguro seguro = avaliadoraSeguro.retornaSeguroAdequadoCliente(cliente);
